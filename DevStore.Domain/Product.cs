9417c09dab3ff3f72c0a65c8d85dc6f376d66712
Guilherme Vada <gvadaguariba@gmail.com>
2020-02-21 00:01:11 -0300

    initial commit

---
+++ DevStore.Domain/Product.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace DevStore.Domain
+{
+    public class Product
+    {
+        public Product()
+        {
+            this.AcquireData = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public DateTime AcquireData { get; set; }
+        public bool IsActive { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+}
